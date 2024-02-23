8a1858938e5fc3e55e3c49d9532fb0426f7e9dea
unknown <ASUS@SG-LAP-12.sgdesign.com>
2024-02-23 15:01:04 +0545

    code optimization

--- src/page/GetValue.ts
+++
@@ -1,9 +0,0 @@
-export const getElementValue = (elementId: string): number => {
-    const element = document.getElementById(elementId) as HTMLInputElement | null;
-    return element ? parseFloat(element.value) : 0;
-};
-
-export const getElementStringValue = (elementId: string): string => {
-    const element = document.getElementById(elementId) as HTMLInputElement | null;
-    return element ? element.value : '';
-};
