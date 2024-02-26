9b259bbc75aae5fcad48aa8f5464e02dcab5c87e
unknown <ASUS@SG-LAP-12.sgdesign.com>
2024-02-26 14:36:54 +0545

    xlsx float data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Car from './page/Car'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Duct from './page/duct-sizer/duct'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Car/>} />
+        <Route path ="duct-size" element={<Duct />} />
     </Routes>
   </BrowserRouter>
   )
---
+++ src/page/duct-sizer/duct.tsx
@@ -0,0 +1,21 @@
+const Duct = () => {
+
+    return (
+        <div>
+        <div className="text-center font-bold text-4xl mt-10 bg-slate-50">
+            Duct Sizer
+        </div>
+        <div>
+        <table>
+            <tr>
+                <th>
+                    
+                </th>
+            </tr>
+        </table>
+        </div>
+        </div>
+    )
+}
+
+export default Duct
