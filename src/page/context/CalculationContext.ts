2dc016c42fb4fa858896eeeb153bad5c364536ec
unknown <ASUS@SG-LAP-12.sgdesign.com>
2024-02-26 13:35:46 +0545

    variable interpretation

--- src/page/context/CalculationContext.ts
+++ src/page/context/CalculationContext.ts
@@ -1,52 +1,131 @@
-// import React, { createContext, useContext, useState, ReactNode, Dispatch, SetStateAction } from "react";
+// import React, { createContext, useContext, useState, ReactNode } from 'react';
 
-// interface CalculationState {
-//   greatestValues: Record<string, number | null>;
-//   combinedGreatestValue: number | null;
-//   inputFactors: Record<string, string>;
-//   inputStaffExposures: Record<string, string>;
-//   inputVehicleTypes: Record<string, string>;
-//   inputValuesP: Record<string, string>;
-//   percentValue: string;
-//   calculatedValues: Record<string, string>;
-//   calculateAirSupply: number;
-// }
-
-// interface CalculationContextType {
-//   state: CalculationState;
-//   setState: Dispatch<SetStateAction<CalculationState>>;
-// }
-
-// const CalculationContext = createContext<CalculationContextType | undefined>(undefined);
-
+// // Create a context
+// const CalculationContext = createContext({});
 // interface CalculationProviderProps {
-//   children: ReactNode;
-// }
+//     children: ReactNode;
+//   }
 
-// export const useCalculationContext = (): CalculationContextType => {
+// // Create a provider component to wrap your app with
+// export const CalculationProvider: React.FC<CalculationProviderProps>  = ({ children }) => {
+//   const [greatestValues, setGreatestValues] = useState({
+//     col1: null,
+//     col2: null,
+//     col3: null,
+//     col4: null,
+//     col5: null,
+//   });
+
+//   const [combinedGreatestValue, setCombinedGreatestValue] = useState(null);
+
+//   const [inputFactors, setInputFactors] = useState({
+//     Fz: '',
+//     Fa: '',
+//     Fb: '',
+//     Fc: '',
+//     Fd: '',
+//   });
+
+//   const [inputStaff, setInputStaff] = useState({
+//     Ez: '',
+//     Ea: '',
+//     Eb: '',
+//     Ec: '',
+//     Ed: '',
+//   });
+
+//   const [inputVType, setInputVType] = useState({
+//     Tz: '',
+//     Ta: '',
+//     Tb: '',
+//     Tc: '',
+//     Td: '',
+//   });
+
+//   const [inputValuesP, setInputValuesP] = useState({
+//     Pz: '',
+//     Pa: '',
+//     Pb: '',
+//     Pc: '',
+//     Pd: '',
+//   });
+
+//   const [percentValue, setPercentValue] = useState('');
+
+//   const [calculatedValues, setCalculatedValues] = useState({
+//     C: '',
+//     Ca: '',
+//     Cb: '',
+//     Cc: '',
+//     Cd: '',
+//   });
+
+//   const [calculatedValuesA, setCalculatedValuesA] = useState({
+//     Az: '',
+//     Aa: '',
+//     Ab: '',
+//     Ac: '',
+//     Ad: '',
+//   });
+
+//   const [calculatedValuesB, setCalculatedValuesB] = useState({
+//     Bz: '',
+//     Ba: '',
+//     Bb: '',
+//     Bc: '',
+//     Bd: '',
+//   });
+
+//   const [calculatedValuesC1, setCalculatedValuesC1] = useState({
+//     C1z: '',
+//     C1a: '',
+//     C1b: '',
+//     C1c: '',
+//     C1d: '',
+//   });
+
+//   const [totalAirSupply, setTotalAirSupply] = useState(null);
+
+//   // Create an object with all the states to provide to consumers
+//   const contextValue = {
+//     greatestValues,
+//     setGreatestValues,
+//     combinedGreatestValue,
+//     setCombinedGreatestValue,
+//     inputFactors,
+//     setInputFactors,
+//     inputStaff,
+//     setInputStaff,
+//     inputVType,
+//     setInputVType,
+//     inputValuesP,
+//     setInputValuesP,
+//     percentValue,
+//     setPercentValue,
+//     calculatedValues,
+//     setCalculatedValues,
+//     calculatedValuesA,
+//     setCalculatedValuesA,
+//     calculatedValuesB,
+//     setCalculatedValuesB,
+//     calculatedValuesC1,
+//     setCalculatedValuesC1,
+//     totalAirSupply,
+//     setTotalAirSupply,
+//   };
+
+//   return (
+//     <CalculationContext.Provider value={contextValue}>
+//       {children}
+//     </CalculationContext.Provider>
+//   );
+// };
+
+// // Custom hook to use the context
+// export const useCalculationContext = () => {
 //   const context = useContext(CalculationContext);
 //   if (!context) {
-//     throw new Error("useCalculationContext must be used within a CalculationProvider");
+//     throw new Error('useCalculationContext must be used within a CalculationProvider');
 //   }
 //   return context;
 // };
-
-// export function CalculationProvider(props: CalculationProviderProps): JSX.Element {
-//   const [state, setState] = useState<CalculationState>({
-//     greatestValues: { col1: null, col2: null, col3: null, col4: null, col5: null },
-//     combinedGreatestValue: null,
-//     inputFactors: { inputFactorFz: "", inputFactorFa: "", inputFactorFb: "", inputFactorFc: "", inputFactorFd: "" },
-//     inputStaffExposures: { inputStaffEz: "", inputStaffEa: "", inputStaffEb: "", inputStaffEc: "", inputStaffEd: "" },
-//     inputVehicleTypes: { inputVTypeTz: "", inputVTypeTa: "", inputVTypeTb: "", inputVTypeTc: "", inputVTypeTd: "" },
-//     inputValuesP: { inputValuePz: "", inputValuePa: "", inputValuePb: "", inputValuePc: "", inputValuePd: "" },
-//     percentValue: "",
-//     calculatedValues: {},
-//     calculateAirSupply: 0,
-//   });
-
-//   return (
-//     <CalculationContext.Provider value={{ state, setState }}>
-//       {props.children}
-//     </CalculationContext.Provider>
-//   );
-// }
