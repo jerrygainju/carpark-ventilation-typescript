925cd3f91f69a452b3ffddf088f179f8553106d5
unknown <ASUS@SG-LAP-12.sgdesign.com>
2024-02-27 13:30:56 +0545

    in progress

--- src/page/duct-sizer/duct.tsx
+++ src/page/duct-sizer/duct.tsx
@@ -19,50 +19,52 @@ const Duct = () => {
     };
 
     return (
-        <div>
-            <div className="text-center font-bold text-4xl pt-10 bg-slate-50">
-                Duct Sizer
-            </div>
-            <div className="flex flex-wrap bg-slate-50 pt-10 justify-center pb-20">
-                <div className="bg-blue-300 w-[500px] h-[600px] rounded">
+        <div className="flex flex-wrap pt-10 justify-center pb-20 bg-cover" style={{ backgroundImage: 'url("https://img.freepik.com/free-photo/beautiful-view-construction-site-city-during-sunset_181624-9347.jpg?w=996&t=st=1709018835~exp=1709019435~hmac=cbc5adcba33f7641a18280d192837a8616ed372068724541d676ab69df2e18f8")' }}>
+            <div className='flex flex-col'>
+                <div className='text-center text-3xl font-bold pb-4 font-serif text-gray-300'>
+                    Duct Size Calculation
+                </div>
+                <div className="bg-orange-300 w-[500px] h-[800px] rounded">
                     <div className='flex flex-row gap-10'>
-                        <div className=' flex flex-col pt-4 pl-6'>
+                        <div className=' flex flex-col pt-6 pl-6'>
                             Units
                             <Select
                                 options={ductOptions}
                                 placeholder="Select Unit"
+                                className='pt-2'
                                 styles={{
                                     control: (provided) => ({
                                         ...provided,
                                         width: '150px',
-                                        backgroundColor: 'rgba(222,221,217,255)',
+                                        backgroundColor: 'rgb(255, 216, 155)',
                                         borderColor: 'transparent',
                                     }),
                                     menu: (provided) => ({
                                         ...provided,
                                         width: '150px',
-                                        backgroundColor: "rgba(222,221,217,255)"
+                                        backgroundColor: "rgb(255, 216, 155)"
                                     }),
                                 }}
                                 onChange={handleUnitChange}
                             />
                         </div>
-                        <div className='pt-4'>
+                        <div className='pt-6'>
                             Material
                             <Select
                                 options={materialOptions}
                                 placeholder="Select Material"
+                                className='pt-2'
                                 styles={{
                                     control: (provided) => ({
                                         ...provided,
                                         width: '165px',
-                                        backgroundColor: 'rgba(222,221,217,255)',
+                                        backgroundColor: 'rgb(255, 216, 155)',
                                         borderColor: 'transparent',
                                     }),
                                     menu: (provided) => ({
                                         ...provided,
                                         width: '165px',
-                                        backgroundColor: "rgba(222,221,217,255)"
+                                        backgroundColor: "rgb(255, 216, 155)"
                                     }),
                                 }}
                                 value={materialOptions.find(option => option.value === inputValue)}
@@ -79,7 +81,7 @@ const Duct = () => {
                     <div className='flex flex-col pl-6 gap-2 pr-6'>
                         <b> Parameter </b>
                         <div>
-                            Airflow: <input placeholder='flow rate' style={{ backgroundColor: 'rgba(222,221,217,255)' }} className='rounded w-28 h-8 text-center' />
+                            Airflow: <input placeholder='flow rate' style={{ backgroundColor: 'rgb(255, 216, 155)' }} className='rounded w-28 h-8 text-center' />
                             {selectedUnit === 'Metric' ? ' L/s' : ' cfm'}
                         </div>
                         <hr className="my-4 border-black " />
@@ -117,7 +119,7 @@ const Duct = () => {
                             <div>
                                 {selectedOption === "velocity" && (
                                     <div>
-                                        Velocity: <input placeholder='add velocity' style={{ backgroundColor: 'rgba(222,221,217,255)' }} className='rounded w-28 h-8 text-center' />
+                                        Velocity: <input placeholder='add velocity' style={{ backgroundColor: 'rgb(255, 216, 155)' }} className='rounded w-28 h-8 text-center' />
                                         {selectedUnit === 'Metric' ? ' M/s' : ' fpm'}
                                     </div>
                                 )}
@@ -125,7 +127,7 @@ const Duct = () => {
                             <div>
                                 {selectedOption === "frictionLoss" && (
                                     <div>
-                                        Friction Loss: <input placeholder='add friction loss' style={{ backgroundColor: 'rgba(222,221,217,255)' }} className='rounded w-32 h-8 text-center' />
+                                        Friction Loss: <input placeholder='add friction loss' style={{ backgroundColor: 'rgb(255, 216, 155)' }} className='rounded w-32 h-8 text-center' />
                                         {selectedUnit === 'Metric' ? ' Pa/30m' : ' in. wg/100'}
                                     </div>
                                 )}
